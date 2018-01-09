1a5c6863e7b47ff45e6a1a497b56c24bda1269cb
Mark Heath <mark.heath@gmail.com>
2018-01-09 22:11:51 +0000

    Adding a basic function chaining orchestrator function

---
+++ DurableFunctionVideoProcessor/ProcessVideoOrchestrator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+
+namespace DurableFunctionVideoProcessor
+{
+    public static class ProcessVideoOrchestrator
+    {
+        [FunctionName("ProcessVideoOrchestrator")]
+        public static async Task<object> Run(
+            [OrchestrationTrigger] DurableOrchestrationContext ctx,
+            TraceWriter log)
+        {
+            var videoLocation = ctx.GetInput<string>();
+            var transcodedLocation = await ctx.CallActivityAsync<string>
+                                                ("TranscodeVideo", videoLocation);
+            var thumbnailLocation = await ctx.CallActivityAsync<string>
+                                                ("ExtractThumbnail", transcodedLocation);
+            var withIntroLocation = await ctx.CallActivityAsync<string>
+                                                ("PrependIntro", transcodedLocation);
+            return new { transcodedLocation, thumbnailLocation, withIntroLocation };
+        }
+    }
+}
