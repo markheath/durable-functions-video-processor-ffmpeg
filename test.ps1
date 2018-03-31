894cb4396d97a13b7b682a01820e3100a91d630a
Mark Heath <mark.heath@gmail.com>
2018-03-31 21:16:05 +0100

    fixed show history output command

--- test.ps1
+++ test.ps1
@@ -47,7 +47,7 @@ Invoke-RestMethod -Uri $orchestrationInfo.statusQueryGetUri
 
 # https://docs.microsoft.com/en-us/azure/azure-functions/durable-functions-http-api
 Start-Process "$($orchestrationInfo.statusQueryGetUri)&showHistory=true" 
-Start-Process -Uri "$($orchestrationInfo.statusQueryGetUri)&showHistory=true&showHistoryOutput=true" 
+Start-Process "$($orchestrationInfo.statusQueryGetUri)&showHistory=true&showHistoryOutput=true" 
 
 # send an approval
 $orchestrationId = $orchestrationInfo.id
